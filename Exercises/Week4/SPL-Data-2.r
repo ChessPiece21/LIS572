b3ecd2a59d218c4515421beb5b3189b6aaba7f85
Joe Lollo <53008442+ChessPiece21@users.noreply.github.com>
2022-10-19 17:25:01 -0700

    Added new answer to the question

--- Exercises/Week4/SPL-Data-2.r
+++ Exercises/Week4/SPL-Data-2.r
@@ -41,12 +41,22 @@ publication_years <- spl_data %>% count(PublicationYear)
 #From SPL report: all organized by bibliographic record -- [] printing date, (c) copyright date
 
 # What do you notice about the publication year data? What steps should we take next? Should we clean these values?
+# I notice that there is a big difference between the way years were written in 
+#the dataset. As we discussed in class, that's mostly because of the different
+#types of years mentioned #in the documentation, such as publication and
+#copyright years, BUT that is not apparent in the spreadsheet without looking
+#at the documentation. While I see many arguments FOR cleaning the data, I also
+#think that inconsistencies in data, like the SPL set, can help you answer #questions about time, place, and subject, which can be useful for working 
+#with more humanistic questions related to your code. DH is all about 
+#preserving data as much as it is working with it from a technical perspective.
 
 ############ Most Checked Out Items
 
 # What are the most checked out items in this SPL data?
+# How many times has each title been checked out?
 # To answer this question, you need to make a dataframe that shows how many times each individual item has been checked out
 # Save your new dataframe as a variable called most_checked_out
-most_checked_out <- spl_data %>% group_by(Title) %>% summarize(sum(Checkouts))
+most_checked_out <- spl_data %>% group_by(Title) %>%
+                    summarize(sum(Checkouts))
 
 
