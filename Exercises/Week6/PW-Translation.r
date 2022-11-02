ad7d1eca4713fb9ff0bd372fd920b9e981837661
Joe Lollo <53008442+ChessPiece21@users.noreply.github.com>
2022-11-02 11:30:33 -0700

    Create PW-Translation.r

---
+++ Exercises/Week6/PW-Translation.r
@@ -0,0 +1,41 @@
+## Exploring Books in Translation
+
+# Load libraries
+library(dplyr)
+library(ggplot2)
+install.packages("plotly")
+library(plotly)
+
+## Exercise 1: Load the Publisher's Weekly Translation dataset from the URL below and save it to a variable called `pw_df`
+# https://github.com/melaniewalsh/Neat-Datasets/blob/main/PW-Translation.csv?raw=true
+# Hint: This CSV file is separated by tabs, not commas
+# You might need to read the documentation for read.csv() or google this question
+pw_df <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/blob/main/PW-Translation.csv?raw=true", stringsAsFactors = FALSE, sep = "\t") #to read in tab-separated or deliminated files.
+help(read.csv) # To help me.
+
+## Take a moment to view and inspect the data
+## Try to find a book that you are familiar with
+
+## Exercise 2: How many books from each country had works translated into English since 2008?
+# Make a dataframe with the total number of books published from each country
+# Save as books_per_country
+books_per_country <- pw_df %>% group_by(country) %>%
+                     summarize(total.books = n())
+
+# France has the most.
+
+## Exercise 3: 
+## Slice the dataframe books_per_country for only the top 10 countries with the most books
+# Save as top_10_countries
+top_10_countries <- books_per_country %>%
+                    slice_max(n = 10, order_by = total.books)
+
+help(slice_max)
+
+## Exercise 4: Make an *interactive* bar plot of the 10 countries translated into English 
+# Bonus: customize the interactive tooltip in some way
+barplot <- ggplot(data = top_10_countries) +
+geom_col(mapping = aes(x = country, y= total.books,
+fill = country))
+
+ggplotly(barplot)
