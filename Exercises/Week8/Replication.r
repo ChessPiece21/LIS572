eccc71e7a14c6380fefa6bc6aa7b0099dfcb1534
Joe Lollo <53008442+ChessPiece21@users.noreply.github.com>
2022-11-14 15:20:54 -0800

    Create Replication.r

---
+++ Exercises/Week8/Replication.r
@@ -0,0 +1,16 @@
+# Replication Experiment for Fleshing Out Models of Gender
+
+# Load replication data from Fleshing Out Models of Gender
+# https://dataverse.harvard.edu/file.xhtml?persistentId=doi:10.7910/DVN/QUGW8V/S6YFQ9&version=1.0
+gender_data <- read.csv("https://github.com/melaniewalsh/Neat-Datasets/raw/main/Figure1Data.csv", stringsAsFactors = FALSE)
+
+
+# Exercise 1: See if you can recreate this plot with ggplot
+# You are plotting the percentange of characterization that is bodily description over time
+# This percentage should be in the "percentage" column
+# Note: You might have to google new things or look at the ggplot documentation to add shapes and smoothing lines
+library("ggplot2")
+
+ggplot(data = gender_data) +
+  geom_point(aes(x = pub_date, y = percentage, shape = gender, color = gender)) + 
+  geom_smooth(aes(x = pub_date, y = percentage, color = gender))
