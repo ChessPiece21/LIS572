d693370293d816120358ae5bfe2cc526fc9adfcc
Joe Lollo <53008442+ChessPiece21@users.noreply.github.com>
2022-11-16 14:52:04 -0800

    Update Tidytext.r

--- Exercises/Week8/Tidytext.r
+++ Exercises/Week8/Tidytext.r
@@ -8,7 +8,7 @@
 # Load TED Talk data
 ted_talks <- read.csv("https://raw.githubusercontent.com/kinnaird-laudun/data/main/Release_v0/TEDonly_speakers_final.csv", stringsAsFactors = FALSE)
 
-install.packages("tidytext")
+#install.packages("tidytext")
 library(tidytext)
 library(tidyverse)
 library(dplyr)
@@ -27,15 +27,18 @@ install.packages("stopwords")
 library("stopwords")
 
 en_stopwords <- data.frame(word = stopwords(language = "en", source = "snowball"))
-
 zh_stopwords <- data.frame(word = stopwords(language = "zh", source = "misc"))
-
 es_stopwords <- data.frame(word = stopwords(language = "es"))
 
-View(en_stopwords)
-View(zh_stopwords)
-View(es_stopwords)
+en_stopwords
+zh_stopwords
+es_stopwords
 
 # Exercise 3: Remove stopwords from word_count
 word_count_no_stops <- tidy_talks %>% anti_join(get_stopwords()) %>%
                        count(word, sort = TRUE)
+
+# Exercise 4: Filter for the top X number of words
+top_words <- word_count_no_stops %>% filter(n > 4707) 
+top_words
+#To get the top 10 words
