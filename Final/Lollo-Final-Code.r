c0d6d9452b6641fb311fb38b7571a47875fb2fa2
Joe Lollo <53008442+ChessPiece21@users.noreply.github.com>
2022-11-27 06:26:15 -0800

    Update Lollo-Final-Code.r

--- Final/Lollo-Final-Code.r
+++ Final/Lollo-Final-Code.r
@@ -12,17 +12,16 @@ library("plotly")
 ow2_df <- read.csv("ow2-2022.csv", stringsAsFactors = FALSE)
 
 ## Part 1: OW2 Heroes by Pick Rate.
-# Filter using DPLYR so that the top 10 heroes appear in the final graph.
+# Filter using DPLYR so that the top 10 Heroes appear in the final graph.
 top_heroes <- ow2_df %>% filter(Pick_Rate > 15) # Estimating the highest pick rates using a benchmark.
 
-# Create interactive plot of basic pick rates among the top in-game Heroes.
+# Create interactive plot of basic pick rates among the top Heroes.
 top_heroes_plot <- ggplot(top_heroes) +
   geom_col(aes(x = Pick_Rate, y = reorder(Hero, +Pick_Rate), fill = Role)) +
   labs(x = "Pick Rate", y = "Hero Name", title = "Highest Play Rates of Overwatch 2 Heroes")
 
 # Make the plot interactive. 
 ggplotly(top_heroes_plot)
-# It's interesting that Hanzo's the top! Definitely something I'll write about in the final blog, because he's considered one of the most balanced and beginner-friendly characters.
 
 ## Part 2: Average Pick Rates by Hero Role.
 # Group the original data frame by Hero role, calculate the mean pick rate and save it in a new variable.
@@ -52,14 +51,13 @@ gender_plot <- ggplot(heroes_by_gender) +
   geom_col(aes(x = Avg_Pick_Rate, y = reorder(Gender, +Avg_Pick_Rate), fill = Gender)) +
   labs(x = "Average Pick Rate", y = "Hero Gender", title = "Average Play Rate by Hero Gender")
 
-# Make the plot interactive.
 ggplotly(gender_plot)
 
 # Now let's look at the bottom pick rates. I kinda want to see this distribution...
 ## Part 4: Bottom 10 Pick Rates.
 bottom_heroes <- ow2_df %>% filter(Pick_Rate < 7.15) # Estimating the lowest pick rates using a benchmark.
 
-# Create interactive plot of basic pick rates among the top in-game Heroes.
+# Create interactive plot of basic pick rates among the bottom in-game Heroes.
 bottom_heroes_plot <- ggplot(bottom_heroes) +
   geom_col(aes(x = Pick_Rate, y = reorder(Hero, -Pick_Rate), fill = Role)) +
   labs(x = "Pick Rate", y = "Hero Name", title = "Lowest Play Rates of Overwatch 2 Heroes")
@@ -69,4 +67,18 @@ ggplotly(bottom_heroes_plot)
 
 # Now THAT is interesting! Even if Offense heroes in OW2 have the highest average pick rate, an Offense hero has the lowest pick rate of all!
 # Sombra is considered by fans to be one of the "most annoying" characters to fight against in-game because of her abilities, but the data doesn't show that at all, just that she's among the lowest picked.
-# (Also, Echo is one of the characters I main, I'm offended that they're among the lowest, because they're so fun to use...)
+# (Also, Echo is one of the characters I main, I'm offended...)
+
+# My last question has to do with Play of the Game (POTG) award rates as they relate to pick rates, because I wonder if the highest picked Heroes also have the highest picked POTGs.
+## Part 5: Top Play of the Game Rates.
+# Filter using DPLYR so that the Heroes with the top 10 POTG rates appear in the final graph.
+top_potgs <- ow2_df %>% filter(POTG_Rate > 10.54) # Using the actual 10th data point.
+
+# Create interactive plot of POTG rates.
+potg_plot <- ggplot(top_potgs) + 
+  geom_col(aes(x = POTG_Rate, y = reorder(Hero,+POTG_Rate), fill = Role)) + 
+  labs(x = "POTG Rate", y = "Hero Name", title = "Top Play of the Game Rates in Overwatch 2")
+
+ggplotly(potg_plot)
+# Interesting, only half of the top picked heroes have the top POTG rates.
+# Also Support heroes are really being done dirty in this plot lmao. 
