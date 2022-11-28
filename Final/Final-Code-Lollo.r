65fdbd5754242c175ce1217c817ce332177afdf8
Joe Lollo <53008442+ChessPiece21@users.noreply.github.com>
2022-11-28 15:03:55 -0800

    Added API links

--- Final/Final-Code-Lollo.r
+++ Final/Final-Code-Lollo.r
@@ -118,6 +118,7 @@ heroes_by_role <- ow2_df %>% group_by(Role) %>% summarize(Avg_Pick_Rate = mean(P
 hero_role_plot <- ggplot(heroes_by_role) +
   geom_col(aes(x = Avg_Pick_Rate, y = reorder(Role, +Avg_Pick_Rate), fill = Role)) +
   labs(x = "Average Play Rate", y = "Hero Role", title = "Average Play Rate by Hero Role") +
+  coord_flip() +
   scale_fill_brewer(palette = "Set1")
 
 # Make the plot interactive.
@@ -173,3 +174,10 @@ ggplotly(bottom_heroes_plot)
 # Upload ggplotly plots to the Plotly API:
 Sys.setenv("plotly_username" = "JoeLollo21")
 Sys.setenv("plotly_api_key" = "StG9GebmLPqXjbdj7kyB")
+
+api_create(top_heroes_plot, filename = "Top OW2 Heroes")
+api_create(bottom_heroes_plot, filename = "Bottom OW2 Heroes")
+api_create(gender_plot, filename = "OW2 Play Rate By Gender")
+api_create(hero_role_plot, filename = "OW2 Play Rate By Roles")
+api_create(hero_boxplot, filename = "Hero Role Boxplot")
+api_create(gender_boxplot, filename = "Hero Gender Boxplot")
