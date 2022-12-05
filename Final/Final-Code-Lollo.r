8d152363e2a6d1d512f3560cdae1f44b038c7a4a
Joe Lollo <53008442+ChessPiece21@users.noreply.github.com>
2022-12-05 15:09:29 -0800

    Update data category for pick rate
    Changed "pick rate" to "play rate" to better represent the project.

--- Final/Final-Code-Lollo.r
+++ Final/Final-Code-Lollo.r
@@ -1,6 +1,6 @@
 ## LIS 572 Final Data Analysis: Overwatch 2 Hero Play Rates
 # by Joe Lollo
-# Version 7: updated December 5, 2022
+# Version 8: updated December 5, 2022
 
 # Load all relevant libraries
 #install.packages(tidyverse) # Once per machine.
@@ -26,36 +26,40 @@ ow2_df <- read.csv("https://raw.githubusercontent.com/ChessPiece21/LIS-572/main/
 # I also cleaned the data, as I had to change the names of Soldier.76 and D.va, as they appear in the original dataset and in-game, to "Soldier 76" and "D-va," 
 #since the R console was having difficulty recognizing those characters as names.
 
-## Part 1: OW2 Heroes by Pick Rate.
-# Filter using DPLYR so that the top 10 Heroes appear in the final graph.
-top_heroes <- ow2_df %>% filter(Pick_Rate > 15) # Estimating the highest pick rates using a benchmark.
+## Part 1: OW2 Heroes by Play Rate.
+# For the rest of the analysis, since I say "PLAY rate" and not "pick rate" in my graphs, I will
+#rename the column as appropriate.
+ow2_df <- ow2_df %>% rename(Play_Rate = Play_Rate)
 
-# Create interactive plot of basic pick rates among the top Heroes.
+# Filter using DPLYR so that the top 10 Heroes appear in the final graph.
+top_heroes <- ow2_df %>% filter(Play_Rate > 15) # Estimating the highest play rates using a benchmark.
+
+# Create interactive plot of basic play rates among the top Heroes.
 top_heroes_plot <- ggplot(top_heroes) +
-  geom_col(aes(x = Pick_Rate, 
-               y = reorder(Hero, +Pick_Rate), 
+  geom_col(aes(x = Play_Rate, 
+               y = reorder(Hero, +Play_Rate), 
                fill = Role,
                text = paste0("Hero Name: ", Hero,
                              "\nHero Role: ", Role,
-                             "\nPlay Rate: ", Pick_Rate,"%"))) +
+                             "\nPlay Rate: ", Play_Rate,"%"))) +
   labs(x = "Play Rate", y = "Hero Name", title = "Highest Play Rates of Overwatch 2 Heroes") +
   scale_fill_brewer(palette = "Set1")
 
 # Make the plot interactive. 
 ggplotly(top_heroes_plot, tooltip = "text")
 
-## Part 2: Average Pick Rates by Hero Role.
-# Group the original data frame by Hero role, calculate the mean pick rate and save it in a new variable.
-heroes_by_role <- ow2_df %>% group_by(Role) %>% summarize(Count = n(), Avg_Pick_Rate = mean(Pick_Rate))
+## Part 2: Average Play Rates by Hero Role.
+# Group the original data frame by Hero role, calculate the mean play rate and save it in a new variable.
+heroes_by_role <- ow2_df %>% group_by(Role) %>% summarize(Count = n(), Avg_Play_Rate = mean(Play_Rate))
 
-# Create interactive plot of Hero pick rate by role.
+# Create interactive plot of Hero play rate by role.
 hero_role_plot <- ggplot(heroes_by_role) +
-  geom_col(aes(x = Avg_Pick_Rate, 
-               y = reorder(Role, +Avg_Pick_Rate), 
+  geom_col(aes(x = Avg_Play_Rate, 
+               y = reorder(Role, +Avg_Play_Rate), 
                fill = Role,
                text = paste0("Hero Role: ", Role,
                              "\nNumber of Heroes: ", Count,
-                             "\nAverage Play Rate: ", Avg_Pick_Rate,"%"))) + 
+                             "\nAverage Play Rate: ", Avg_Play_Rate,"%"))) + 
   labs(x = "Average Play Rate", y = "Hero Role", title = "Average Play Rate by Hero Role") +
   scale_fill_brewer(palette = "Set1")
 
@@ -65,7 +69,7 @@ ggplotly(hero_role_plot, tooltip = "text")
 # Already interesting that Offense heroes have the highest, as expected, but DEFENSE, out of the other 3, is nearing -- is this because of two of them (Hanzo and Bastion) being known outliers?
 
 # This begs the question...what would OW2_DF grouped by role look like in a boxplot?
-hero_boxplot <- ggplot(ow2_df, aes(x = Role, y = Pick_Rate, color = Role)) + geom_boxplot() +
+hero_boxplot <- ggplot(ow2_df, aes(x = Role, y = Play_Rate, color = Role)) + geom_boxplot() +
   labs(x = "Role", y = "Play Rate", title = "Distribution of Play Rates by Hero Role")  +
   coord_flip() +
   scale_color_brewer(palette = "Set1")
@@ -73,8 +77,8 @@ hero_boxplot <- ggplot(ow2_df, aes(x = Role, y = Pick_Rate, color = Role)) + geo
 ggplotly(hero_boxplot) # Yep, I knew it -- Defense has a wider range than the rest. Based on the dataset, Mercy is an outlier among 
 #Support Heroes since she is widely used and considered an asset on teams.
 
-## Part 3: Average Pick Rates by Gender.
-# Make new data frame grouping Heroes by gender without pick rate, just to see the count of each gender.
+## Part 3: Average Play Rates by Gender.
+# Make new data frame grouping Heroes by gender without play rate, just to see the count of each gender.
 gender_df <- ow2_df %>% group_by(Gender) %>% summarize(Count = n())
 
 gender_count_plot <- ggplot(gender_df) +
@@ -89,18 +93,18 @@ gender_count_plot <- ggplot(gender_df) +
 
 ggplotly(gender_count_plot, tooltip = "text")
 
-# More female Heroes than male Heroes, despite being nearly equal, but are their pick rates this close, too, on average?
+# More female Heroes than male Heroes, despite being nearly equal, but are their play rates this close, too, on average?
 
-# Group the original data frame by Hero gender, calculate the mean pick rate and save it in a new variable.
-heroes_by_gender <- ow2_df %>% group_by(Gender) %>% summarize(Avg_Pick_Rate = mean(Pick_Rate))
+# Group the original data frame by Hero gender, calculate the mean play rate and save it in a new variable.
+heroes_by_gender <- ow2_df %>% group_by(Gender) %>% summarize(Avg_Play_Rate = mean(Play_Rate))
 
-# Create interactive plot of pick rate by gender.
+# Create interactive plot of play rate by gender.
 gender_plot <- ggplot(heroes_by_gender) +
-  geom_col(aes(x = Avg_Pick_Rate, 
-               y = reorder(Gender, +Avg_Pick_Rate), 
+  geom_col(aes(x = Avg_Play_Rate, 
+               y = reorder(Gender, +Avg_Play_Rate), 
                fill = Gender,
                text = paste0("Hero Gender: ", Gender,
-                             "\nAverage Play Rate: ", Avg_Pick_Rate,"%"))) +
+                             "\nAverage Play Rate: ", Avg_Play_Rate,"%"))) +
   labs(x = "Average Play Rate", y = "Hero Gender", title = "Average Play Rate by Hero Gender")  +
   coord_flip() +
   scale_fill_brewer(palette = "Set1")
@@ -108,26 +112,26 @@ gender_plot <- ggplot(heroes_by_gender) +
 ggplotly(gender_plot, tooltip = "text")
 
 # Let's see what happens if we view this as a boxplot!
-gender_boxplot <- ggplot(ow2_df, aes(x = Gender, y = Pick_Rate, color = Gender)) + geom_boxplot() +
+gender_boxplot <- ggplot(ow2_df, aes(x = Gender, y = Play_Rate, color = Gender)) + geom_boxplot() +
   labs(x = "Role", y = "Play Rate", title = "Distribution of Play Rates by Hero Gender") +
   coord_flip() +
   scale_color_brewer(palette = "Set1")
 
 ggplotly(gender_boxplot) # Females have the widest range, but males have the highest average.
 
-# Now let's look at the bottom pick rates. I kinda want to see this distribution...
+# Now let's look at the bottom play rates. I kinda want to see this distribution...
 
-## Part 4: Bottom 10 Pick Rates.
-bottom_heroes <- ow2_df %>% filter(Pick_Rate < 7.15) # Estimating the lowest pick rates using a benchmark.
+## Part 4: Bottom 10 Play Rates.
+bottom_heroes <- ow2_df %>% filter(Play_Rate < 7.15) # Estimating the lowest play rates using a benchmark.
 
-# Create interactive plot of basic pick rates among the bottom in-game Heroes.
+# Create interactive plot of basic play rates among the bottom in-game Heroes.
 bottom_heroes_plot <- ggplot(bottom_heroes) +
-  geom_col(aes(x = Pick_Rate, 
-               y = reorder(Hero, -Pick_Rate), 
+  geom_col(aes(x = Play_Rate, 
+               y = reorder(Hero, -Play_Rate), 
                fill = Role,
                text = paste0("Hero Name: ", Hero,
                              "\nHero Role: ", Role,
-                             "\nPlay Rate: ", Pick_Rate,"%"))) +
+                             "\nPlay Rate: ", Play_Rate,"%"))) +
   labs(x = "Play Rate", y = "Hero Name", title = "Lowest Play Rates of Overwatch 2 Heroes") +
   scale_fill_brewer(palette = "Set1") +
   scale_x_continuous(limits = c(0, 10))
@@ -135,7 +139,7 @@ bottom_heroes_plot <- ggplot(bottom_heroes) +
 # Make the plot interactive. 
 ggplotly(bottom_heroes_plot, tooltip = "text")
 
-# Now THAT is interesting! Even if Offense heroes in OW2 have the highest average pick rate, an Offense hero has the lowest pick rate of all!
+# Now THAT is interesting! Even if Offense heroes in OW2 have the highest average play rate, an Offense hero has the lowest play rate of all!
 # Sombra is considered by fans to be one of the "most annoying" characters to fight against in-game because of her abilities, but the data doesn't show that at all, just that she's among the lowest picked.
 
 ## Final Extra Part: Uploading interactive plots for my blog.
